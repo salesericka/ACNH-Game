f8671f83ddf54f7f98ccd276e44ba6aa825b6083
Paulo <paulovlutz@gmail.com>
2020-06-05 13:34:54 -0400

    updating files

--- src/pages/GamePage/GamePage.js
+++ src/pages/GamePage/GamePage.js
@@ -84,30 +84,30 @@ class GamePage extends Component {
                         })}
                 </div>
 
-            <div className="game">
-                <div className="game-card">
-                    <div className="game-card__image">
-                        "Image"
-                    </div>
-                    <div className="game-card__question">
-                        "Question"
-                    </div>
-                    <div className="game-card__answer-wrapper">
-                        <p className="game-card__answer game-card__answer-one">
-                            "Answer"
-                        </p>
-                        <p className="game-card__answer game-card__answer-two">
-                            "Answer"
-                        </p>
-                        <p className="game-card__answer game-card__answer-three">
-                            "Answer"
-                        </p>
-                        <p className="game-card__answer game-card__answer-four">
-                            "Answer"
-                        </p>
+                <div className="game">
+                    <div className="game-card">
+                        <div className="game-card__image">
+                            "Image"
+                        </div>
+                        <div className="game-card__question">
+                            "Question"
+                        </div>
+                        <div className="game-card__answer-wrapper">
+                            <p className="game-card__answer game-card__answer-one">
+                                "Answer"
+                            </p>
+                            <p className="game-card__answer game-card__answer-two">
+                                "Answer"
+                            </p>
+                            <p className="game-card__answer game-card__answer-three">
+                                "Answer"
+                            </p>
+                            <p className="game-card__answer game-card__answer-four">
+                                "Answer"
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
             </>
         )
     }
