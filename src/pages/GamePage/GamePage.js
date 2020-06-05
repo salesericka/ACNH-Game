182f945e16bad2d2b470f1f06dbc300019986298
Paulo <paulovlutz@gmail.com>
2020-06-05 15:53:22 -0400

    fixed minor bugs

--- src/pages/GamePage/GamePage.js
+++ src/pages/GamePage/GamePage.js
@@ -70,16 +70,22 @@ class GamePage extends Component {
         if (name === this.state.villager.name) {
             this.setState(prevState=>{
                 return{
-                    villager: {},
-                    villagerNames: [],
                     userPoint: prevState.userPoint + 1,
                     message:`You guessed it right! This is ${name}.`
                 }
             })
-            this.getVillager();
-            this.getOtherVillager();
-            this.getOtherVillager();
-            this.getOtherVillager();
+            setTimeout(() => {
+                this.setState({
+                    villager: {},
+                    villagerNames: [],
+                    message: ""
+                })
+                this.getVillager();
+                this.getOtherVillager();
+                this.getOtherVillager();
+                this.getOtherVillager();
+            }, 3000);
+
         } else {
             this.setState(prevState=>{
                 return{
@@ -155,12 +161,12 @@ class GamePage extends Component {
         return (
             <div className="game">
                 <div className="game-card">
-                    <div className="game-card__image-wrapper">
-                        <Countdown date={Date.now() + 15000} onComplete={() => this.timeIsOut()} autoStart={true} key={this.state.round} />
+                    <div className="game-card__image-wrapper timer">
+                        <h2><Countdown date={Date.now() + 15000} onComplete={() => this.timeIsOut()} autoStart={true} key={this.state.round} renderer={({ hours, minutes, seconds, completed }) => <span>{seconds} seconds left</span>} /></h2>
 
                         <img src={this.state.villager.image} className="game-card__image">
                         </img>
-                        <p>{this.state.message}</p>
+                        <p className>{this.state.message}</p>
                     <div className="game-card__question-wrapper">
                         <img src={this.state.villager.icon} className="game-card__icon"/>
                         <p className='game-card__question'>
