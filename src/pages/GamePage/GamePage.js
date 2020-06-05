b713d54b13bf064ea09dd5db14c0643a2d16b6bb
Paulo <paulovlutz@gmail.com>
2020-06-05 11:45:53 -0400

    added function to call other villagers names

--- src/pages/GamePage/GamePage.js
+++ src/pages/GamePage/GamePage.js
@@ -6,16 +6,18 @@ const API_URL = 'http://acnhapi.com/v1/villagers/';
 
 class GamePage extends Component {
     state = {
-        villager: {}
+        villager: {},
+        villagerNames: []
     }
 
-    getVillagers = () => {
-        let villagerId = (Math.floor(Math.random() * 391) + 1)
+    getVillager = () => {
+        let villagerId = (Math.floor(Math.random() * 391) + 1);
 
         axios
             .get(API_URL + villagerId)
             .then(result => {
                 let villagerName = result.data.name['name-USen'];
+                console.log(villagerName);
                 let villagerImage = result.data.image_uri;
                 this.setState({
                     villager: {
@@ -29,16 +31,43 @@ class GamePage extends Component {
             })
     }
 
-    componentDidMount() {
-        this.getVillagers();
+    getOtherVillagers = () => {
+        let otherVillagerId = (Math.floor(Math.random() * 391) + 1);
+
+        axios
+            .get(API_URL + otherVillagerId)
+            .then(result => {
+                let otherVillagerName2 = result.data.name['name-USen'];
+                let otherVillagerName3 = result.data.name['name-USen'];
+                let otherVillagerName4 = result.data.name['name-USen'];
+
+                this.setState({
+                    villagerNames: [
+                        otherVillagerName2, otherVillagerName3, otherVillagerName4
+                    ]
+                })
+            })
     }
 
+    letsPlay() {
+
+    }
+
+    componentDidMount() {
+        this.getVillager();
+        this.getOtherVillagers();
+    }
 
     render() {
         return (
             <>
                 <div>
                     <img src={this.state.villager.image}></img>
+                    {this.state.villagerNames.map(name => {
+                        return (
+                            <p>{name}</p>
+                        )
+                        })}
                 </div>
             </>
         )
